31e587e1c6f1df2acc863e68a43eaea1e874a4db
BaxanAcharya <baxanacharya@gmail.com>
2020-01-21 10:34:50 +0545

    'checking wifi connection'

--- app/src/main/java/com/biplav/notifications/MainActivity.java
+++ app/src/main/java/com/biplav/notifications/MainActivity.java
@@ -6,7 +6,7 @@ import androidx.core.app.NotificationManagerCompat;
 
 import android.app.Notification;
 import android.os.Bundle;
-import android.view.Display;
+//import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 
